c54db8903bda49907b7fef0351118810a18e1282
Laguna <laguna_999@web.de>
2014-03-02 16:03:06 +0100

    trying to figure out timers. Not Working. Adding WorldSize and Image Origin

--- Assets/JS/game.js
+++ Assets/JS/game.js
@@ -1,53 +1,85 @@
 
 var game = new Phaser.Game(800, 600, '', Phaser.AUTO, { preload: preload, create: create, update: update });
 
+var image;
+var cursors;
+
+var posX = 1;
+var posY = 1;
+var WorldSizeX = 15;
+var WorldSizeY = 15;
+
+var hexagonSize = 25;
+var inputTimer;
+
+
 function preload()
 {
 	game.load.image('player', 'Assets/GFX/player.png');
 }
 
-var image;
-var cursors;
 
-var posX = 0;
-var posY = 0;
-
-var hexagonSize = 25;
 
 function create()
 {
 	image = game.add.sprite(0, 0, 'player');
+	image.anchor = new Phaser.Point(x=0.5, y=0.5);
 	cursors = game.input.keyboard.createCursorKeys();
+	inputTimer = 0;
+}
+
+function DoInputTimer()
+{
+	inputTimer += 0.25;
 }
 
 function getInput()
 {
 	if (cursors.up.isDown)
     {
-     
-        posY -= 1;
+    	DoInputTimer();
+    	if(posY>1)
+    	{
+        	posY -= 1;
+    	}
     }
-	if (cursors.down.isDown)
+	else if (cursors.down.isDown)
     {
-     
-        posY += 1;
+    	DoInputTimer();
+    	if(posY<=WorldSizeY)
+    	{
+        	posY += 1;
+        }
     }
 
     if (cursors.left.isDown)
     {
-     
-        posX -= 1;
+    	DoInputTimer();
+    	if(posX > 1)
+    	{
+        	posX -= 1;	
+    	}
     }
-	if (cursors.right.isDown)
+	else if (cursors.right.isDown)
     {
-     
-        posX += 1;
+    	DoInputTimer();
+    	if(posX<=WorldSizeX)
+    	{
+        	posX += 1;
+        }
     }
 }
 
+
+
+
 function update()
 {
-
+	console.log(inputTimer);
+	if(inputTimer >= 0)
+	{
+		inputTimer -= game.time.elapsed;
+	}
 	image.x = posX * hexagonSize;
 	
 
