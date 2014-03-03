dbbc68965ec21fb83a416501a8273d2a498d9bce
Julian <julian.dinges@gmail.com>
2014-03-03 19:41:49 +0100

    made graphics object public

--- Assets/JS/hexagon.js
+++ Assets/JS/hexagon.js
@@ -3,7 +3,7 @@ function Hexagon(x, y, game, p)
     this.x = x;
     this.y = y;
 
-    var graphics = game.add.graphics(this.x, this.y);
+    this.graphics = game.add.graphics(this.x, this.y);
 
     var offX = this.x * p.size;
     var offY = this.y * p.size + this.y * p.h;
@@ -14,16 +14,16 @@ function Hexagon(x, y, game, p)
         offX += 0;
     }
     
-    graphics.beginFill(0xFF0000);
-    graphics.lineStyle(1, 0xFF0000, 1);
+    this.graphics.beginFill(0xFF0000);
+    this.graphics.lineStyle(1, 0xFF0000, 1);
     
     // Draw the hexagon shape
-    graphics.moveTo(offX + p.h,       offY);
-    graphics.lineTo(offX + p.h + p.s, offY);
-    graphics.lineTo(offX + p.size,    offY + p.r);
-    graphics.lineTo(offX + p.h + p.s, offY + p.size);
-    graphics.lineTo(offX + p.h,       offY + p.size);
-    graphics.lineTo(offX,             offY + p.r);
-    graphics.lineTo(offX + p.h,       offY);
-    graphics.endFill();
+    this.graphics.moveTo(offX + p.h,       offY);
+    this.graphics.lineTo(offX + p.h + p.s, offY);
+    this.graphics.lineTo(offX + p.size,    offY + p.r);
+    this.graphics.lineTo(offX + p.h + p.s, offY + p.size);
+    this.graphics.lineTo(offX + p.h,       offY + p.size);
+    this.graphics.lineTo(offX,             offY + p.r);
+    this.graphics.lineTo(offX + p.h,       offY);
+    this.graphics.endFill();
 }
